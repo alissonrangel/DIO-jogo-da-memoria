ddee872202f14b1ef08ef726d9efb4afbdf46048
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-16 22:33:28 -0300

    alterações no script.js

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const cards = document.querySelectorAll('.card');
 
-function flipCard() {
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+function flipCard() {
     //adiciona a classe flip ao elemento card clicado
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+    unflipCards();
 }
 
 cards.forEach((card)=>{
