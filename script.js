171fffdd340df40bf0ed6917c3e48f967ae1841e
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-17 10:09:32 -0300

    parte 6 resetBoard

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let lockBoard = false;
 
 function flipCard() {
     if(lockBoard) return;
+
+    if (this === firstCard) return;
+
     //adiciona a classe flip ao elemento card clicado
     this.classList.add('flip');
     
@@ -31,13 +34,24 @@ function checkForMath() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);    
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card)=>{
